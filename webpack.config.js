4c8e299c226b49b52d53e02a8c61480297f04314
Shawn Becker <sbecker11@me.com>
2024-09-13 23:06:43 -0600

    updated all-dependencies.json

--- webpack.config.js
+++ webpack.config.js
@@ -49,20 +49,18 @@ module.exports = {
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
-      directory: path.join(__dirname, 'dist'),
       publicPath: '/workspace-rolodex/',  // Ensure this matches the output publicPath
     },
     compress: true,
     port: 9000,
     open: true,  // Automatically open the browser
     hot: true, // Enable Hot Module Replacement
-    proxy: [
-      {
-        context: ['/api'],
+    proxy: {
+      '/api': {
         target: 'http://localhost:5000',  // The backend server URL
         changeOrigin: true,
         pathRewrite: { '^/api': '' }  // Remove /api prefix when forwarding
       }
-    ]
+    }
   },
 };
